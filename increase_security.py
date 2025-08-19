c9e36f190adbe2137a7dcf111d3ad2975e9be6bf
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-08-19 23:13:35 +0530

    increase_security

--- increase_security.py
+++ increase_security.py
@@ -1,6 +1,24 @@
-client_name = input('Enter Your name:')
+client_name = input("Enter the client name: ")
+phone_number = input("Enter the phone number: ")
+email = input("Enter the email address: ")
 
+#security checks for the Client field
 if any(char.isdigit() for char in client_name):
-    print('Name should not contain numbers.')
+    print("Error: Client name should not contain numbers.")
 else:
-    print(client_name)
+    print("Client name is valid.")
+    
+# security checks for the Phone Number field
+if phone_number.isdigit() and len(phone_number) == 10:
+    print("Phone number is accepted:",phone_number)
+else:
+    print("Error: Phone number must be 10 digits long and contain only numbers.")     
+    
+# security checks for the Email field
+if '@gmail.com' in email or '@yahoo.com' in email:
+    print("Email is accepted:", email)
+else:
+    print("Error: Email must be a Gmail or Yahoo address.")
+
+
+    
